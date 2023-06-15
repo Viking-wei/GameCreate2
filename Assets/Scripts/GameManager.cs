0f58315a5e2a5c7bfbef8feb0769ce0476bc071c
Viking-wei <2521616835@qq.com>
2023-06-15 23:34:03 +0800

    移动模式修正，增加了UI提示

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,29 +15,18 @@ public class GameManager : Singleton<GameManager>, ISerializationCallbackReceive
 {
     
     [HideInInspector] public Vector3 playerPosition;
-    
-    #region The player's attributes
-    //TODO:need to fix the initial value
-    private readonly int _attack=3;
-    private readonly int _health=3;
-    public int Attack=>_attack + parserCodeFragmentNum % 3;
-    public int Health=> _health + encryptionAlgorithmCodeFragmentNum % 3;
+    public GameObject messagePrefab;
 
-    [HideInInspector]public int parserCodeFragmentNum = 0;
-    [HideInInspector]public int encryptionAlgorithmCodeFragmentNum = 0;
-    #endregion
-
-    
     private const int CoffeeSceneIndex = 0;
     private const int NightSceneIndex = 1;
     private const int NewsNum=13;
-
+    
     public static int Paragraph=0;
-
+    
     private Animator _transitionAnimator;
-
+    private RectTransform _theGameUIRectTransform;
     //对话存储系统
-    public Dictionary<string, DialogStorage> dialogStorageDictionary;
+    [HideInInspector]public Dictionary<string, DialogStorage> dialogStorageDictionary;
     //NPC名单
     public List<string>nameOfNpc;
     //NPC对话索引记录
@@ -54,15 +43,26 @@ public class GameManager : Singleton<GameManager>, ISerializationCallbackReceive
         NpcDialogIndex=new Dictionary<string, int>();
         SceneManager.activeSceneChanged+=FindFadedCanvas;
     }
+    
 
     private void FindFadedCanvas(Scene current, Scene next)
     {
-        _transitionAnimator=GameObject.Find("ScenesChangeTransition").GetComponent<Animator>();
+        /*_transitionAnimator=GameObject.Find("ScenesChangeTransition").GetComponent<Animator>();*/
+        _theGameUIRectTransform = GameObject.Find("UI").GetComponent<RectTransform>();
 
         if(next.buildIndex==CoffeeSceneIndex)
         {
             Paragraph++;
         }
+    }
+    
+    public void SendMessageToUI(string message)
+    {
+        Vector2 screenPos = new Vector2(0,_theGameUIRectTransform.rect.height/2);
+        GameObject messageObject=Instantiate(messagePrefab, _theGameUIRectTransform);
+        var rectTransform = messageObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = screenPos;
+        messageObject.GetComponentInChildren<TextMeshProUGUI>().text=message;
     }
 
     #region Change Scene APIs
@@ -132,6 +132,17 @@ public class GameManager : Singleton<GameManager>, ISerializationCallbackReceive
     {
         UnitySerializationUtility.SerializeUnityObject(this, ref this.serializationData);
     }
+#endregion
+
+#region The player's attributes
+    //TODO:need to fix the initial value
+    private readonly int _attack=3;
+    private readonly int _health=3;
+    public int Attack=>_attack + parserCodeFragmentNum % 3;
+    public int Health=> _health + encryptionAlgorithmCodeFragmentNum % 3;
+
+    [HideInInspector]public int parserCodeFragmentNum = 0;
+    [HideInInspector]public int encryptionAlgorithmCodeFragmentNum = 0;
 #endregion
 
 }
